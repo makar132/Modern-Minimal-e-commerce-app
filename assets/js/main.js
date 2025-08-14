9dd6e692cdb23031287169610fe81b19970291ef
mego <makarsamer@gmail.com>
2025-08-14 07:05:07 +0300

    feat: initialize Firebase in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 // main.js — entry point (placeholder). Route to page initializers here when implementing.
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyAcictnKPsnIq7Udyiz2XGIuMo72dRdlvU",
+  authDomain: "e-commerce-js-app.firebaseapp.com",
+  projectId: "e-commerce-js-app",
+  storageBucket: "e-commerce-js-app.firebasestorage.app",
+  messagingSenderId: "383942089852",
+  appId: "1:383942089852:web:99224fda7fd586e7cafdf9",
+  measurementId: "G-TV0G1TS360",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
