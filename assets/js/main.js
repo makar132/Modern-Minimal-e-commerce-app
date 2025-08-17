69e169ba8126cda2c9e2084bd1fa4c1110f38ff4
hossam1711 <hossamhy9@gmai.com>
2025-08-17 15:32:25 +0300

    رسالة تشرح التغيير اللي عملته

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAcictnKPsnIq7Udyiz2XGIuMo72dRdlvU",
   authDomain: "e-commerce-js-app.firebaseapp.com",
   projectId: "e-commerce-js-app",
-  storageBucket: "e-commerce-js-app.appspot.com", // ✅ الصح
+  storageBucket: "e-commerce-js-app.appspot.com", 
   messagingSenderId: "383942089852",
   appId: "1:383942089852:web:99224fda7fd586e7cafdf9",
   measurementId: "G-TV0G1TS360",
